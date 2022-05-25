617db06fdb28bb572c2b16947d0092bb46e043b7
Hady Achkar <hady.achkar@hotmail.com>
2022-05-25 14:22:04 +0300

    test

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,7 +9,7 @@ import Validateuser from "../middleware/Validateuser"
 const router = Router()
 
 router.route("/").post(AddNewFunnelController)
-router.route("/:page?").get(BrowseFunnelController)
+router.route("/:page").get(BrowseFunnelController)
 router.route("/publish").put(PublishPageController)
 
 export default router

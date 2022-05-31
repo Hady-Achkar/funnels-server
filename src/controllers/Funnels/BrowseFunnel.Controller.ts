a2bc804dad983e0c8e6bc43ac44c2902423db599
Hady Achkar <hady.achkar@hotmail.com>
2022-05-31 12:31:16 +0300

    test

--- src/controllers/Funnels/BrowseFunnel.Controller.ts
+++ src/controllers/Funnels/BrowseFunnel.Controller.ts
@@ -1,16 +1,16 @@
-import { Request, Response } from "express"
-import path from "path"
-import { Funnels } from "../../models"
+import { Request, Response } from "express";
+import path from "path";
+import { Funnels } from "../../models";
 
 type QueyFunnelHost = {
-  baseDomain?: String
-  proDomain?: String
-}
+  baseDomain?: String;
+  proDomain?: String;
+};
 
 export default async (req: Request, res: Response) => {
   try {
-    const pageKey = req.params.page
-    const host = req.headers.host
+    const pageKey = req.params.page;
+    const host = req.headers.host;
 
     if (!host) {
       return res.status(400).json({
@@ -22,12 +22,12 @@ export default async (req: Request, res: Response) => {
           },
         ],
         requestTime: new Date().toISOString(),
-      })
+      });
     }
 
     const domainRegex =
-      /^(?:[a-zA-Z0-9\-]*[a-zA-Z0-9]\.)*([a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9])(?:\.[a-zA-Z]{2,})+$/
-    const hostRegex = domainRegex.exec(host)
+      /^(?:[a-zA-Z0-9\-]*[a-zA-Z0-9]\.)*([a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9])(?:\.[a-zA-Z]{2,})+$/;
+    const hostRegex = domainRegex.exec(host);
 
     if (!hostRegex) {
       return res.status(400).json({
@@ -39,64 +39,65 @@ export default async (req: Request, res: Response) => {
           },
         ],
         requestTime: new Date().toISOString(),
-      })
+      });
     }
-    const domainHost = hostRegex[1]
-    const query: QueyFunnelHost = {}
+    const domainHost = hostRegex[1];
+    const query: QueyFunnelHost = {};
 
     if (domainHost === "funnelshero-website") {
-      query.baseDomain = `${host.split(".")[0]}.funnelshero-website.com`
+      query.baseDomain = `${host.split(".")[0]}.funnelshero-website.com`;
     } else {
-      query.proDomain = host
+      query.proDomain = host;
     }
 
-    const funnel = await Funnels.findOne(query).populate("pages")
+    const funnel = await Funnels.findOne(query).populate("pages");
 
     if (!funnel) {
       return res.status(404).json({
         status: "Failure",
         message: "Funnel was not found",
         requestTime: new Date().toISOString(),
-      })
+      });
     }
 
-    const filePath = path.join(__dirname, `../../../views/pages`)
+    const filePath = path.join(__dirname, `../../../views/pages`);
     // page.isPublished
     // if req.params.page =="" or / ==> index homeage
 
-    let page
+    let page;
 
-    console.log(pageKey)
+    console.log(pageKey);
 
     if (!pageKey) {
-      page = funnel.pages.find((page) => page.title == "Home")
+      page = funnel.pages.find((page) => page.title == "Home");
     } else {
-      page = funnel.pages.find((page) => page.title === pageKey)
+      page = funnel.pages.find((page) => page.title === pageKey);
     }
 
     if (!page) {
-      return res.render(`${filePath}/404.ejs`, { title: "OPPSS!" })
+      return res.render(`${filePath}/404.ejs`, { title: "OPPSS!" });
     }
 
     const headerMenue =
-      funnel.menus.find((menu) => menu.title === "Header") || []
+      funnel.menus.find((menu) => menu.title === "Header") || [];
     const footerMenue =
-      funnel.menus.find((menu) => menu.title === "Footer") || []
+      funnel.menus.find((menu) => menu.title === "Footer") || [];
 
     return res.render(`${filePath}/index.ejs`, {
       html: page.html,
       headerMenue,
       footerMenue,
       title: page.title,
-    })
+      funnelTitle: funnel.title,
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     if (err instanceof Error) {
       return res.status(500).json({
         message: "Internal Server Error",
         error: err.message,
         requestTime: new Date().toISOString(),
-      })
+      });
     }
   }
-}
+};
